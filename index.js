c0b329a3c6f3ceee95f71899baa48eb5d3bc9788
Nadir Sarigul <nadir.nyc62@gmail.com>
2019-01-30 15:59:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function destructivelyRemoveFirstKitten(){
 function appendKitten(name){
   return [...kittens, name]
 }
-function appendKitten(name){
+function prependKitten(name){
   return [name, ...kittens]
 }
